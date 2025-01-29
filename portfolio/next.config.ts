eae29eecb3cf6c4b701829c561309824afd34495
Julián García <leonardojl14g@gmail.com>
2025-01-29 16:54:21 -0500

    Update next.config.ts

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // Exporta como archivos estáticos
-    // basePath: '/portfolio', // Esto es necesario si tu repositorio no es un user/organization repo
+    basePath: '/portfolio', // Esto es necesario si tu repositorio no es un user/organization repo
     assetPrefix: '/portfolio', // Ruta base para los recursos estáticos
 };
 
